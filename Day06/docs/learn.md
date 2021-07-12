4981e3dfebea8872cf8512884eba0d9963a3eaea
Prem Kumar Palanichamy <premkumar.palanichamy@yahoo.com>
2021-07-13 00:41:18 +0200

    100days of python coding learning - Day06

---
+++ Day06/docs/learn.md
@@ -0,0 +1,3 @@
+> You can find a lot of funcoding tasks and sample output for some can be found under ./Day06/docs/images
+
+<img src="./docs/images/sample.png">
---
+++ Day06/function.py
@@ -0,0 +1,30 @@
+## Sample function creation
+def laya():
+    print("My name is Prem")
+    print("I am a Software Engineer")
+laya()
+
+def func():
+    name = "My name is Prem"
+    tellme = input("What you do? \n")
+    print(name, tellme)
+func()
+
+## Extra tip:
+
+## What is function in python?
+## A function is a block of organized, reusable code that is used to perform a single, related action. Functions provide better modularity for your application and a high degree of code reusing.
+## Python gives you many built-in functions like print(), etc. but you can also create your own functions. These functions are called user-defined functions.
+
+## How to create own function in python?
+## Function blocks begin with the keyword def followed by the function name and parentheses ( ( ) ). The code block within every function starts with a colon (:) and is indented.
+
+## Indentation - Indentation refers to the spaces at the beginning of a code line. Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
+## Python uses indentation to indicate a block of code.
+
+## What is while loop?
+## The while loop in Python is used to iterate over a block of code as long as the test expression (condition) is true.
+
+## What is Infinite loop?
+## An Infinite Loop in Python is a continuous repetitive conditional loop that gets executed until an external factor interferes in the execution flow, like insufficient CPU memory,
+## a failed feature/ error code that stopped the execution, or a new feature in the other legacy systems that needs code integration.
